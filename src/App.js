ec50abd178ec06c6379c8c26c1965465ab8b1d65
andrewksloan <andrewksloan3@gmail.com>
2021-05-06 14:48:11 -0700

    Test modification.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test application to demo AWS-Amplify.
         </p>
         <a
           className="App-link"
